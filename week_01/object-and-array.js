302e2417b878ae6de0532decb16cbd6bf5e33d91
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-03-06 14:02:59 +0700

    add JS file

---
+++ week_01/object-and-array.js
@@ -0,0 +1,5 @@
+let learner = {name:"Ploy", age:24, isGraduate:true,hobbies:["Read kindle","Watch Youtube","Drink Boba milk tea"]};
+console.log(learner.name);
+console.log(learner.age);
+console.log(learner.isGraduate);
+console.log(learner.hobbies[0]);
